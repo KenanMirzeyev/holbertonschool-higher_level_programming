be2b0a207b761cc2434b6fd43545ccee87ce55f4
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-09-06 19:30:42 +0400

    100-my_calculator.py

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    ope = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if ope == '+':
+        result = add(a, b)
+    elif ope == '-':
+        result = sub(a, b)
+    elif ope == '*':
+        result = mul(a, b)
+    elif ope == '/':
+        result = div(a, b)
+    else:
+        print('Unknown operator.Avaible operators: +, -, * and /')
+        sys.exit(1)
+
+    print('{} {} {} = {}'.format(a, ope, b, result))
