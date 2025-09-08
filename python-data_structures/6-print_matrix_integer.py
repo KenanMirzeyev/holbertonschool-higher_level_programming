d32daa36cf2d72afe30537eb8d97ada9cc35062a
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-09-08 17:13:55 +0400

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix or matrix == [[]]:
+        return
     for row in matrix:
         for i, elem in enumerate(row):
             if i != len(row) - 1:
