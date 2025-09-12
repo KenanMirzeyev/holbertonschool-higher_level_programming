7fd5e244841128faa13a51e265fc8878c655e450
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-09-12 11:00:11 +0000

    100-weight_average.py

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    t_w_s = 0
+    t_w = 0
+    for score, weight in my_list:
+        t_w_s += score * weight
+        t_w += weight
+    
+    return t_w_s / t_w
