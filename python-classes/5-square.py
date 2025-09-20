f460a7b5684ab89016676105be2303390e1dc150
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-09-20 12:15:05 +0000

    5-

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -10,11 +10,11 @@ class Square:
     """
     def __init__(self, size=0):
         self.size = size
-    
+
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -22,13 +22,13 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         return self.__size ** 2
-    
+
     def my_print(self):
         if self.__size == 0:
             print()
             return
         for _ in range(self.__size):
-            print("#" * self.__size )
+            print("#" * self.__size)
