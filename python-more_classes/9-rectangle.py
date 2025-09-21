94f1ba4c939357ae918afb7c7bdaf7e8dbea4a9f
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-09-21 16:07:46 +0000

    .

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
             return 0
         else:
             return 2 * (self.width + self.height)
-    
+
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
