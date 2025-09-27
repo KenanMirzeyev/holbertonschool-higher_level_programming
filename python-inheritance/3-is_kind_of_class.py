8d10256049e43ff8567083d12a2b0f7b8dda5c8c
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-09-27 10:01:10 +0000

    3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Inheritance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    L
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
