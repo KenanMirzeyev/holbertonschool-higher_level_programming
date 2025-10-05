9ebea46eb901731035b5d2c2cb922dd25b71b888
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-10-05 11:31:24 +0000

    1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+writes a string to a string
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    write_file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
