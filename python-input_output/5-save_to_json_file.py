1dbdc3df1f84ea3fbb85107c69262c9d682c91e3
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-10-05 15:28:21 +0000

    7-add_item.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     """
     A
     """
-    with open(filename, "w") as json.file:
-        json.dump(my_obj, json.file, indent=4)
+    with open(filename, "w", encoding="udf-8") as json.file:
+        json.dump(my_obj, json.file)
