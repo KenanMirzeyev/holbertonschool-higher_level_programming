97e29cf31ea9383b2266c671b83d6e2910a173a0
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-10-05 15:49:40 +0000

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 """
-A
+Module 10-student
+Defines a Student class with optional attribute filtering for JSON representation.
 """
 
 
 class Student:
-    """
-    A
-    """
+    """Defines a student with first_name, last_name, and age."""
+
     def __init__(self, first_name, last_name, age):
-        """
-        A
-        """
+        """Initialize a Student instance with first_name, last_name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """
-        A
+        Return a dictionary representation of the Student instance.
+        If attrs is a list of strings, only include attributes in that list.
+        Otherwise, return all attributes.
         """
         if isinstance(attrs, list):
             f_dict = {}
-            for i in attrs:
-                if i in self.__dict__:
-                    f_dict[k] = self.__dict__[key]
+            for key in attrs:
+                if key in self.__dict__:
+                    f_dict[key] = self.__dict__[key]
             return f_dict
         return self.__dict__.copy()
