84a7dbacf58cb7b24fdeab2f0e9e5b2bf18fd6ac
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-10-05 16:48:59 +0000

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
 12-pascal_triangle.py
-Returns a list of lists representing Pa
+Returns a list of lists representing Pascal's Triangle of n
 """
 
-
 def pascal_triangle(n):
-    """Generate Pascal’s triangle of n rows."""
+    """Generate Pascal's triangle of n rows."""
     if n <= 0:
         return []
 
@@ -14,7 +13,12 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         prev_row = triangle[-1]
-        row = [1] + [prev_row[j] + prev_row[j + 1] for j in range(len(prev_row) - 1)] + [1]
+        row = (
+            [1]
+            + [prev_row[j] + prev_row[j + 1]
+               for j in range(len(prev_row) - 1)]
+            + [1]
+        )
         triangle.append(row)
 
     return triangle
