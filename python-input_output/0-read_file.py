86bf469d813ae885bd662106f8d3766f715849a6
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-10-05 11:14:49 +0000

    my_file_0.txt

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+reading a file
+"""
 def read_file(filename=""):
+    """
+    def
+    """
     with open("my_file_0.txt", mode="r", encoding="utf-8"):
         my_file_0.txt.read()
