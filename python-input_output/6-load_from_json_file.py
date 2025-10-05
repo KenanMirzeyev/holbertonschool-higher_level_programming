4cebaff2c7c28246ad622669a7bbc0902bf5c5be
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-10-05 11:57:20 +0000

    6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -11,5 +11,5 @@ def load_from_json_file(filename):
     """
     A
     """
-    with open(filename) as json.file:
-        json.load(json.file)
+    with open(filename, "r") as json.file:
+        return json.load(json.file)
