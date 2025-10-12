81cd23af9522196171f09e5ee46af9f8dcbfafad
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-10-12 20:02:58 +0000

    main_02_requests.py

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+def fetch_and_print_posts():
+    r = "https://jsonplaceholder.typicode.com/posts"
+    respond = requests.get(r)
+    print("Status Code:", respond.status_code)
+
+    if respond.status_code == 200:
+        post = respond.json()
+        for pos in post:
+            print(pos['title'])
+
+def fetch_and_save_posts():
+    r = "https://jsonplaceholder.typicode.com/posts"
+    respond = requests.get(r)
+
+    if respond.status_code == 200:
+        post = respond.json()
+
+        structured = [
+            {'id': pos['id'], 'title': pos['title'], 'body': pos['body']}
+            for pos in post
+        ]
+
+        with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(structured)
