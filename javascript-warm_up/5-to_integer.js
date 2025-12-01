e31f23d639547f27cea13e0dafb583634bc7ff8b
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-12-01 20:11:31 +0000

    5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const a = process.argv[2];
 const n = parseInt(a);
 
 if (isNaN(n)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number:', n);
+  console.log('My number:', n);
 }
