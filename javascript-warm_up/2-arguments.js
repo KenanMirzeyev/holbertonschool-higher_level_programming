ced30d56b0b336ceae1dfd565a40a1e58ea1c3a5
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-12-01 19:55:49 +0000

    2-arguments.js

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const acount == process.argv.lenght - 2;
+
+if (acount === 0) {
+	console.log('No argument');
+} else if (acount === 1) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
