7ac956968445d71d6b46fafec492a1a88d51abe0
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-12-05 18:32:08 +0000

    9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,8 +2,8 @@
 const a = parseInt(process.argv[2], 10);
 const b = parseInt(process.argv[3], 10);
 
-function add(a, b) {
-	return a + b
+function add (a, b) {
+  return a + b;
 }
 
-console.log(add(a,b))
+console.log(add(a, b));
