b069987b091e14f469247d06258c8a639fc92702
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-12-05 18:26:56 +0000

    8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ const x = process.argv[2];
 const y = parseInt(x, 10);
 
 if (isNaN(y)) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else if (y > 0) {
-	for (let i = 0; i < y; i++) {
-		console.log('X'.repeat(y));
-	}
+  for (let i = 0; i < y; i++) {
+    console.log('X'.repeat(y));
+  }
 }
