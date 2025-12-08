24d763f208450894eb62d35aa291dc67adc83f85
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-12-08 18:55:58 +0000

    11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+"""Module"""
+
+const args = process.argv.slice(2).map(Number);
+
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  const max = Math.max(...args);
+  const filtered = args.filter(n => n !== max);
+
+  if (filtered.length === 0) {
+    console.log(0);
+  } else {
+    const second = Math.max(...filtered);
+    console.log(second);
+  }
